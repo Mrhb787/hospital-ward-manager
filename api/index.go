8c41d6e454abf14608c459ec900d6f5ac97e17d7
mrhb787 <abhishekworks787@gmail.com>
2025-03-15 18:33:38 +0530

    minor server changes

--- api/index.go
+++ api/index.go
@@ -3,11 +3,18 @@ package api
 import (
 	"net/http"
 
+	"github.com/Mrhb787/hospital-ward-manager/common"
 	"github.com/Mrhb787/hospital-ward-manager/service/http/health"
 	"github.com/Mrhb787/hospital-ward-manager/transport"
 )
 
 func Health(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path == "/" {
+		// Serve index.html
+		common.ServeIndexHTML(w, r)
+		return
+	}
 
 	// initalize services
 	healthService := health.NewService()
